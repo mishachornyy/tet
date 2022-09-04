baffe4f1b58735428df52798cc741b1ddf920232
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2022-09-04 16:12:25 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Menu from './component/Menu'
+import './App.css';
+
+function App() {
+  return (
+    <> <Menu />
+      <div className="bg-green-300">
+      <h1>test3</h1></div></>
+  );
+
+}
+
+export default App;
+
+
